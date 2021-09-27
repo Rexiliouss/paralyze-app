5440ac3ff0c30b0d65e0b3d70f7a7ef3409f21c3
crkmuhammet <41701987+Rexiliouss@users.noreply.github.com>
2021-09-27 16:28:43 +0300

    Add files via upload

---
+++ paralyze/src/main/java/com/paralyze/paralyze/ParalyzeApplication.java
@@ -0,0 +1,13 @@
+package com.paralyze.paralyze;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParalyzeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParalyzeApplication.class, args);
+	}
+
+}
---
+++ paralyze/src/test/java/com/paralyze/paralyze/ParalyzeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paralyze.paralyze;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParalyzeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
