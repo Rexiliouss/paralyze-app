7fe0beb960ee90a51cb504d02b4c0a8809f26df9
crkmuhammet <crk.muhammet@gmail.com>
2021-10-06 15:40:48 +0300

    Sil baştan
    En baştan

--- paralyze/src/main/java/com/paralyze/paralyze/Controller/UserController.java
+++ paralyze/src/main/java/com/paralyze/paralyze/Controller/UserController.java
@@ -1,15 +1,12 @@
 package com.paralyze.paralyze.Controller;
 
-import com.paralyze.paralyze.Dto.CreateUserRequest;
 import com.paralyze.paralyze.Model.User;
-import com.paralyze.paralyze.Shared.ApiStatusBuilder;
+import com.paralyze.paralyze.Repository.UserRepository;
 import com.paralyze.paralyze.Service.UserService;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
 
 
 @RestController
@@ -22,14 +19,14 @@ public class UserController {
 
     //User Creator
     @PostMapping("/signup")
-    public ApiStatusBuilder<Object> saveUser(@Valid @RequestBody CreateUserRequest createUserRequest){
-        return new ApiStatusBuilder(HttpStatus.OK,"Kayıt oluşturuldu.","/api/v1/users/signup",this.userService.createUser(createUserRequest));
+    public User saveUser(@Valid @RequestBody User user){
+        return this.userService.createUser(user);
     }
 
     //List All users
     @GetMapping("/allusers")
-    public ApiStatusBuilder<Object> findAllUsers(){
-        return new ApiStatusBuilder(HttpStatus.OK,"Kayıtlar Listelendi.","/api/v1/users/allusers",this.userService.findAllUsers());
+    public List<User> findAllUsers(){
+        return this.userService.findAllUsers();
     }
 
 }
--- paralyze/src/main/java/com/paralyze/paralyze/Service/UserService.java
+++ paralyze/src/main/java/com/paralyze/paralyze/Service/UserService.java
@@ -1,45 +1,24 @@
 package com.paralyze.paralyze.Service;
 
-import com.paralyze.paralyze.Dto.CreateUserRequest;
-import com.paralyze.paralyze.Dto.UserDto;
-import com.paralyze.paralyze.Dto.UserDtoConverter;
-import com.paralyze.paralyze.Shared.ApiStatusBuilder;
 import com.paralyze.paralyze.Model.User;
 import com.paralyze.paralyze.Repository.UserRepository;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 public class UserService {
-    UserRepository userRepository;
-    UserDtoConverter userDtoConverter;
+    private UserRepository userRepository;
 
-
-    public UserService(UserRepository userRepository,UserDtoConverter userDtoConverter){
+    public UserService(UserRepository userRepository){
         this.userRepository=userRepository;
-        this.userDtoConverter=userDtoConverter;
-    }
-    public UserDto createUser(CreateUserRequest createUserRequest) {
-        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-        User user = new User(createUserRequest.getUserId(),
-                createUserRequest.getUserName(),
-                createUserRequest.getDisplayName(),
-                passwordEncoder.encode(createUserRequest.getPassword()));
-        System.out.println("Sisteme Kayıt Yapıldı");
-        return userDtoConverter.converter(this.userRepository.save(user));
     }
 
-    public List<UserDto> findAllUsers() {
-        return userRepository.findAll().
-                stream().map(x -> userDtoConverter.converter(x)).collect(Collectors.toList());
+    public User createUser(User user){
+        return userRepository.save(user);
+    }
+    public List<User> findAllUsers(){
+        return userRepository.findAll();
     }
 
 }
