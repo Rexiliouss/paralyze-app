b97585efe9e1aedc0678c2f1fcd5d80c22c4da4e
crkmuhammet <crk.muhammet@gmail.com>
2021-10-07 15:25:17 +0300

    Login Auth
    Login Auth

--- paralyze/src/main/java/com/paralyze/paralyze/Controller/AuthController.java
+++ paralyze/src/main/java/com/paralyze/paralyze/Controller/AuthController.java
@@ -1,5 +1,6 @@
 package com.paralyze.paralyze.Controller;
 
+import com.paralyze.paralyze.Exception.ApiError;
 import com.paralyze.paralyze.Service.UserService;
 import com.paralyze.paralyze.Shared.GenericResponse;
 import org.springframework.http.HttpStatus;
@@ -11,11 +12,11 @@ import org.springframework.web.bind.annotation.*;
 public class AuthController {
     private UserService userService;
     @PostMapping("/auth")
-    public ResponseEntity<?> loginUser(@RequestHeader(name="Authorization", required = false) String auth){
-        if(auth==null || auth.isBlank() || auth.isBlank()){
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+    public ApiError loginUser(@RequestHeader(name="Authorization", required = false) String auth){
+        if(auth!=null && (auth.isBlank() || auth.isBlank())){
+            return new ApiError(HttpStatus.UNAUTHORIZED,"Yetkilendirme Başarısız","/auth");
         }else{
-            return ResponseEntity.status(HttpStatus.OK).build();
+            return new ApiError(HttpStatus.OK,"Yetkilendirme Başarılı","/auth");
         }
 
     }
